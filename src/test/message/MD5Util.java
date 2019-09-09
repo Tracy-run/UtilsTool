56da7eaac5bd6d097fb7e1eb4ecee34f945cda3e
Tracy--RUN <2463136713@qq.com>
2019-09-09 22:32:34 +0800

    update md5

--- src/test/message/MD5Util.java
+++ src/test/message/MD5Util.java
@@ -3,6 +3,11 @@ package test.message;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
 public class MD5Util {
 
 	    private static String byteArrayToHexString(byte b[]) {
@@ -44,30 +49,105 @@ public class MD5Util {
 	        return MD5Encode(origin, "utf-8");
 	   }
 
-	    private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
+	   static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
 	        "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
-
+	   
+	   static final char hexDigit[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 	    
-	    
-	    //second method
-	    public static String MD5(String str) {
-	        try {
-	            // 生成一个MD5加密计算摘要
-	            MessageDigest md = MessageDigest.getInstance("MD5");
-	            // 计算md5函数
-	            md.update(str.getBytes());
-	            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-	            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-	            String md5=new BigInteger(1, md.digest()).toString(16);
-	            //BigInteger会把0省略掉，需补全至32位
-	            return fillMD5(md5);
-	        } catch (Exception e) {
-	            throw new RuntimeException("MD5加密错误:"+e.getMessage(),e);
-	        }
-	    }
-
-	    public static String fillMD5(String md5){
-	        return md5.length()==32?md5:fillMD5("0"+md5);
-	    }
+    /**
+     * 生成MD5码
+     * 
+     * @param plainText
+     *            要加密的字符串
+     * @return md5值
+     */
+    public final static String MD5(String plainText) {
+        try {
+            byte[] strTemp = plainText.getBytes("UTF-8");
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigit[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigit[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+ 
+    /**
+     * 生成MD5码
+     * 
+     * @param plainText
+     *            要加密的字符串
+     * @return md5值
+     */
+    public final static String MD5(byte[] plainText) {
+        try {
+            byte[] strTemp = plainText;
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigit[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigit[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+ 
+    /**
+     * 先进行HmacSHA1转码再进行Base64编码
+     * @param data  要SHA1的串
+     * @param key   秘钥
+     * @return
+     * @throws Exception
+     */
+    public static String HmacSHA1ToBase64(String data, String key) throws Exception {
+        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");  
+        Mac mac = Mac.getInstance("HmacSHA1");  
+        mac.init(signingKey);  
+        byte[] rawHmac = mac.doFinal(data.getBytes());  
+        return Base64.encodeBase64String(rawHmac);
+    }
+ 
+    /**
+     * 校验MD5码
+     * 
+     * @param text
+     *            要校验的字符串
+     * @param md5
+     *            md5值
+     * @return 校验结果
+     */
+    public static boolean valid(String text, String md5) {
+        return md5.equals(MD5(text)) || md5.equals(MD5(text).toUpperCase());
+    }
+ 
+    /**
+     * 
+     * @param params
+     * @return
+     */
+    public static String MD5(String... params) {
+        StringBuilder sb = new StringBuilder();
+        for (String param : params) {
+            sb.append(param);
+        }
+        return MD5(sb.toString());
+    }
 	    
 }
